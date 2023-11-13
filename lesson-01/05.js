102c48c26115e1251a3da495276196afdc7d1303
Galina3217552 <dashka32175@gmail.com>
2023-11-13 19:13:59 +0300

    add 05.js and change index.html

---
+++ lesson-01/05.js
@@ -0,0 +1,21 @@
+let rule =
+  "Еще не родился тот человек, который поставил бы цель и не смог бы стать программистом праволыдфравыоылфд рпаывперпуамвыы";
+
+switch (rule.length) {
+  case 0:
+    console.error("Строка не может быть пустой");
+    break;
+
+  default:
+  case rule.length < 25:
+    console.log("Все таки нет правил без исключения");
+    break;
+
+  case rule.length > 25:
+    console.log("У меня 100 пудов все получится");
+    break;
+
+  case rule.length === 25:
+    console.log("50 на 50");
+    break;
+}
